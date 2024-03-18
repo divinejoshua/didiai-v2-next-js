10f969189530d5c6f3427a368fe3f9554028783c
divinejoshua <divineuche399@gmail.com>
2024-03-18 16:08:29 +0000

    Added LLM response

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// GROQ
+const Groq = require("groq-sdk");
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
+
+//BING
+const BING_SEARCH_API_KEY = process.env.GROQ_API_KEY
+// const BING_SEARCH_V7_ENDPOINT =  os.environ['BING_SEARCH_V7_ENDPOINT'] +"v7.0/search"
+
+
+
+// POST request
+export async function POST (req: NextRequest, res : NextResponse) {
+
+    //Get the search query
+    const {searchParams} = new URL(req.url);
+    let searchQuery : string | null | undefined
+    searchQuery = searchParams.get("query");
+
+    //LLM handler
+    let llmResponse = await LLMHandler(searchQuery)
+
+    let data = { message: llmResponse }
+
+    //Response
+    return NextResponse.json(data, {
+        status: 200,
+    });
+  }
+
+
+//SEO Handler 
+
+// LLM Handler
+async function LLMHandler(searchQuery : string | null) {
+    const completion = await groq.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content: searchQuery
+            }
+        ],
+        model: "mixtral-8x7b-32768"
+    });
+
+    let response = completion.choices[0]?.message?.content || ""
+    return response
+}
