55a37d9f712665e762fb7bfb8434f89dc15aa8bd
divinejoshua <divineuche399@gmail.com>
2024-03-24 00:31:58 +0000

    Added LLM + SEO summary. Also added links

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -16,13 +16,22 @@ export async function POST (req: NextRequest, res : NextResponse) {
     let searchQuery : string | null | undefined
     searchQuery = searchParams.get("query");
 
-    //LLM handler
-    let llmResponse = await LLMHandler(searchQuery)
-
     //SEO handler
     let seoResponse = await SEOHandler(searchQuery)
 
-    let data = { message: seoResponse }
+    //Get SEO context from name and snippet
+    let searchContextFromSEO = await getSEOKeyDetails(seoResponse)
+
+    //LLM handler
+    let llmResponse = await LLMHandler(searchQuery, searchContextFromSEO)
+
+    //SEO Data
+    let seoLinks = await getSEOLinks(seoResponse)
+
+    let data = {
+        message: llmResponse,
+        links : seoLinks,
+    }
 
     //Response
     return NextResponse.json(data, {
@@ -52,9 +61,13 @@ async function SEOHandler(searchQuery : string | null) {
 }
 
 // LLM Handler
-async function LLMHandler(searchQuery : string | null) {
+async function LLMHandler(searchQuery : string | null, searchContextFromSEO : any) {
     const completion = await groq.chat.completions.create({
         messages: [
+            {
+                role: "system",
+                content: searchContextFromSEO,
+            },
             {
                 role: "user",
                 content: searchQuery
@@ -68,3 +81,37 @@ async function LLMHandler(searchQuery : string | null) {
 }
 
 
+//Get SEO key details
+async function getSEOKeyDetails(seoResponse : any){
+    let serpResults : any = seoResponse.webPages.value
+
+    // Map through the array and extract only 'name' and 'snippet'
+    const filteredData = serpResults.map((item : any) => {
+    return {
+        name: item.name,
+        snippet: item.snippet
+    };
+    });
+    // Convert the data to string
+    const jsonString = JSON.stringify(filteredData);
+
+    return jsonString;
+}
+
+// Get SEO Links
+async function getSEOLinks(seoResponse : any){
+    let serpResults : any = seoResponse.webPages.value
+
+    // Map through the array and extract only 'name' and 'snippet'
+    const filteredData = serpResults.map((item : any) => {
+    return {
+        name: item.name,
+        url: item.url,
+        snippet: item.snippet,
+        datePublished : item.datePublished,
+        dateLastCrawled : item.dateLastCrawled,
+    };
+    })
+
+    return filteredData;
+}
