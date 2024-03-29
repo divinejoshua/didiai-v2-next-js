9196d011e5cc6476651d227add10ffbedbfb68ca
divinejoshua <divineuche399@gmail.com>
2024-03-29 21:52:25 +0000

    Added header logo

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import twitterIcon from "../assets/icons/twitter.svg"
 import githubIcon from "../assets/icons/github.svg"
+import headerLogo from "../assets/logo.svg"
 import Image from 'next/image'
 
 export default function HeaderComponent() {
@@ -9,7 +10,15 @@ export default function HeaderComponent() {
         {/* Navbar */}
         <nav className="nav-header font-bold pl-5 pr-5">
             <div className='nav-div pb-4'>
-                Didi AI <span className='text-blue-500'>Search</span>
+                <div className='flex float-left'>
+                    <Image
+                        src={headerLogo}
+                        alt='Didi AI search'
+                        height={20}
+                        priority={true}
+                    />
+                    Search<span className='text-blue-500'>Didi</span>
+                </div>
                 {/* Link to twitter account  */}
                 <a href='https://twitter.com/Divine_Er' rel="noreferrer" target="_blank" >
                     <span className='float-right'>
