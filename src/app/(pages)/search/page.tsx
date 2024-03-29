64621e7e26d44a83f869a47c2bb4660a4be30651
divinejoshua <divineuche399@gmail.com>
2024-03-29 23:22:47 +0000

    Started search page

---
+++ src/app/(pages)/search/page.tsx
@@ -0,0 +1,14 @@
+import HeaderComponent from '@/app/components/header'
+import React from 'react'
+
+export default function SearchPage() {
+  return (
+    <main className="App mx-auto pl-5 pr-5">
+
+    {/* Header  */}
+    <HeaderComponent/>
+
+    {/* Content */}
+    </main>
+  )
+}
